3fe38e11ec5fd067030c212ec3bcd87609f990af
mih <michael.hoelscher@qplix.com>
2024-06-30 21:46:44 +0200

    Learning Rationals...

--- data_utility.py
+++ data_utility.py
@@ -1,4 +1,6 @@
-from sympy import simplify
+import sympy
+from sympy import simplify, symbols
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,14 +22,12 @@ def dataset_from_formula_string(formula: str, number_of_evals: int, low: float =
     return data
 
 
-def plot_data(data_array):
-    inputs = [pair[:-1] for pair in data_array]
-    outputs = [pair[-1] for pair in data_array]
+def plot_data(inputs, outputs, color='blue'):
     input_dim = len(inputs[0])
     if input_dim == 1:
         # For 1D inputs, create a simple scatter plot
         inputs = [x[0] for x in inputs]  # Unpack the single-element lists
-        plt.scatter(inputs, outputs, c='blue', marker='o')
+        plt.scatter(inputs, outputs, c=color, marker='o')
         plt.xlabel('Input')
         plt.ylabel('Output')
         plt.title('Input vs. Output')
@@ -37,7 +37,7 @@ def plot_data(data_array):
         ax = fig.add_subplot(111, projection='3d')
         xs = [x[0] for x in inputs]
         ys = [x[1] for x in inputs]
-        ax.scatter(xs, ys, outputs, c='blue', marker='o')
+        ax.scatter(xs, ys, outputs, c=color, marker='o')
         ax.set_xlabel('Input 1')
         ax.set_ylabel('Input 2')
         ax.set_zlabel('Output')
@@ -47,8 +47,112 @@ def plot_data(data_array):
     plt.show()
 
 
+def tensor_to_rational_function(coefficients: torch.Tensor):
+    coefficients = coefficients.cpu().numpy()
+    numerator_coeffs, denominator_coeffs = coefficients[:coefficients.size//2], coefficients[coefficients.size//2:]
+
+    _x = symbols('x')
+
+    numerator = sum(coeff.round(decimals=5) * _x ** i for i, coeff in enumerate(reversed(numerator_coeffs)))
+    denominator = sum(coeff.round(decimals=5) * _x ** i for i, coeff in enumerate(reversed(denominator_coeffs)))
+    return simplify(numerator/denominator)
+
+
+def rational_function_to_tensor(rational_func):
+    try:
+        numerator_coeffs, denominator_coeffs = [sympy.Poly(poly).all_coeffs() for poly in sympy.fraction(rational_func)]
+    except sympy.polys.polyerrors.PolynomialError:
+        print(f'Sympy Error: Please only use integers as exponents for your rational function')
+        return
+    len1, len2 = len(numerator_coeffs), len(denominator_coeffs)
+    if len1 > len2:
+        denominator_coeffs = [0] * (len1 - len2) + denominator_coeffs
+    elif len2 > len1:
+        numerator_coeffs = [0] * (len2 - len1) + numerator_coeffs
+
+    return torch.Tensor(numerator_coeffs + denominator_coeffs)
+
+
+def plot_predictions(train_data, train_labels, test_data, test_labels, predictions=None):
+    """
+    Plots training data, test data and compares predictions.
+    """
+    plt.figure(figsize=(10, 7))
+
+    # Plot training data in blue
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+
+    # Plot test data in green
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        # Plot the predictions in red (predictions were made on the test data)
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    # Show the legend
+    plt.legend(prop={"size": 14})
+
+
+def plot_predictions_2d(train_data, train_labels, test_data, test_labels, predictions=None):
+    """
+    Plots training data, test data, and compares predictions for 2D input.
+
+    Args:
+        train_data (array-like): Training data points with 2D input.
+        train_labels (array-like): Labels for the training data.
+        test_data (array-like): Test data points with 2D input.
+        test_labels (array-like): Labels for the test data.
+        predictions (array-like, optional): Predicted labels for the test data.
+    """
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot training data in blue
+    ax.scatter(train_data[:, 0], train_data[:, 1], train_labels, c="b", s=20, label="Training data")
+
+    # Plot test data in green
+    ax.scatter(test_data[:, 0], test_data[:, 1], test_labels, c="g", s=20, label="Testing data")
+
+    if predictions is not None:
+        # Plot the predictions in red (predictions were made on the test data)
+        ax.scatter(test_data[:, 0], test_data[:, 1], predictions, c="r", s=20, label="Predictions")
+
+    # Set labels
+    ax.set_xlabel('X1')
+    ax.set_ylabel('X2')
+    ax.set_zlabel('Labels')
+
+    # Show the legend
+    ax.legend(prop={"size": 14})
+
+    # Show the plot
+    plt.show()
+
+
+def function_to_plot(target_function, predicted_function=None, x_min=-10, x_max=10, num_points=400):
+    # Generate x values
+    x_values = np.linspace(x_min, x_max, num_points)
+
+    # Compute y values for the main function
+    y_values = target_function(x_values)
+
+    # Plot the main function
+    plt.figure(figsize=(8, 6))
+    plt.plot(x_values, y_values, label='Target Function', color='blue')
+
+    # Optionally, plot the predicted_function if provided
+    if predicted_function:
+        y_predicted_values = predicted_function(x_values)
+        plt.plot(x_values, y_predicted_values, label='Predicted Function', linestyle='--', color='red')
+
+    # Add labels, title, grid, and legend
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Plot of Functions')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
 if __name__ == '__main__':
-    function = 'x_0**3'
-    data = dataset_from_formula_string(function, 100, low=-3.0, high=3.0)
-    print(data)
-    plot_data(data)
+    my_function = 'x_0**3'
---
+++ mysr/RationalSR.py
@@ -0,0 +1,114 @@
+import sympy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch import Tensor
+
+import data_utility
+
+
+class RationalFunction(nn.Module):
+    """
+    A PyTorch module that represents and learns a rational function.
+
+    The rational function is of the form P(x) / Q(x), where P(x) and Q(x) are polynomials.
+    The coefficients of these polynomials are learnable parameters.
+
+    Attributes:
+    coeffs_p (nn.Parameter): The coefficients of the numerator polynomial P(x).
+    coeffs_q (nn.Parameter): The coefficients of the denominator polynomial Q(x).
+
+    Methods:
+    forward(x):
+        Computes the forward pass through the rational function.
+    fit(x_train, y_train, num_epochs=100, learning_rate=0.01):
+        Trains the RationalFunction model using the provided training data.
+    get_function():
+        Returns the rational function.
+    """
+    def __init__(self, degree_p=3, degree_q=3):
+        """
+        Initializes the RationalFunction module.
+
+        Parameters:
+        num_coeffs_p (int): The number of coefficients in the numerator polynomial P(x).
+        num_coeffs_q (int): The number of coefficients in the denominator polynomial Q(x).
+        """
+        super(RationalFunction, self).__init__()
+        self.coeffs_p = nn.Parameter(torch.ones(degree_p + 1))
+        self.coeffs_q = nn.Parameter(torch.ones(degree_q + 1))
+
+    def forward(self, x) -> Tensor:
+        """
+        Forward pass through the RationalFunction module.
+
+        Parameters:
+        x (torch.Tensor): The input tensor.
+
+        Returns:
+        torch.Tensor: The output tensor after applying the rational function.
+        """
+        # Compute the numerator P(x)
+        num = sum([self.coeffs_p[i] * x ** i for i in range(len(self.coeffs_p))])
+
+        # Compute the denominator Q(x)
+        denom = sum([self.coeffs_q[i] * x ** i for i in range(len(self.coeffs_q))])
+
+        return torch.Tensor(num / denom)
+
+    def fit(self, x_input, target, num_epochs=1000, learning_rate=0.01):
+        """
+        Fit the RationalFunction model to the training data.
+
+        Parameters:
+        x_train (torch.Tensor): The input training data.
+        y_train (torch.Tensor): The target training data.
+        num_epochs (int): The number of training epochs.
+        learning_rate (float): The learning rate for the optimizer.
+        """
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        model.train()
+        for epoch in range(num_epochs):
+            y_pred = self.forward(x_input)
+            loss = criterion(y_pred, target)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            with torch.no_grad():
+                self.coeffs_p[torch.abs(self.coeffs_p) < 0.1] = 0.0
+                self.coeffs_q[:-1][torch.abs(self.coeffs_q[:-1]) < 0.1] = 0.0
+
+            if (epoch + 1) % 100 == 0:
+                print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+                print(self.get_function())
+
+    def get_function(self):
+        """
+            Constructs a symbolic rational function from the stored numerator and denominator coefficients.
+
+            Returns:
+                sympy.core.expr.Expr: A simplified SymPy expression representing the rational function.
+            """
+        _x = sympy.Symbol('x')
+        numerator = sum(coeff.round(decimals=5) * _x ** i for i, coeff in enumerate(reversed(self.coeffs_p)))
+        denominator = sum(coeff.round(decimals=5) * _x ** i for i, coeff in enumerate(reversed(self.coeffs_q)))
+        return sympy.sympify(numerator / denominator)
+
+
+if __name__ == '__main__':
+    device = 'cpu'
+    model = RationalFunction(2, 2).to(device)
+    x_train = torch.linspace(-10, 10, 500).to(device)
+    target_function = sympy.lambdify('x', sympy.sympify('x**2'))
+    y_train = (x_train**2)
+
+    # Train the model
+    model.fit(x_train, y_train, num_epochs=10000, learning_rate=0.01)
+    model.eval()
+    recovered_function = sympy.lambdify('x', model.get_function())
+    with torch.no_grad():
+        print("Learned coefficients for P(x):", model.coeffs_p)
+        print("Learned coefficients for Q(x):", model.coeffs_q)
+        print("Recovered function: ", model.get_function())
+    data_utility.function_to_plot(target_function, recovered_function)
