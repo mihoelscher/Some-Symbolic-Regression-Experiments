172358e975ce0d67083d476cb2193237c7398420
mihoelscher <michael.hoelscher10@gmail.com>
2024-11-06 22:22:52 +0100

    stuff

--- comparison.py
+++ comparison.py
@@ -1,24 +1,30 @@
 import os
+import time
+
 import sympy
+import torch
+
 from data_utility import process_tensor_with_function, create_input_tensor
 import pandas as pd
 
 if __name__ == '__main__':
-    usage = ['ginnlp']
+    usage = ['ginnlp', "parfam", 'mysr']
+    # usage = ['ginnlp']
     device = 'cpu'
-    function = '2*x_0**3 + 3.14159*x_1'
-    override_results = False
+    function = 'x_0'
+    override_results = True
     # parfam fail,
     # ginnlp close: 0.064/(X_0**0.088*X_1**0.09) + 2.001*X_0**3 + 3.014*X_1**0.998
     file = "comparison_table.csv"
-    results = pd.Series([function, 'not yet computed', 'not yet computed', 'not yet computed'],
-                        index=['target', 'parfam', 'ginnLP', 'mySR'])
+    RESULT_COLUMNS = ['target', 'parfam', 'parfam_time', 'ginnLP', 'ginnLP_time', 'mySR', 'mySR_time']
+    results = pd.Series([function, 'not yet computed', 'not yet computed', 'not yet computed', 'not yet computed', 'not yet computed', 'not yet computed'],
+                        index=RESULT_COLUMNS)
     if os.path.exists(file):
         comparison_table = pd.read_csv(file)
     else:
-        comparison_table = pd.DataFrame(columns=['target', 'parfam', 'ginnLP', 'mySR'])
+        comparison_table = pd.DataFrame(columns=RESULT_COLUMNS)
 
-    x_train = create_input_tensor(function, 1000)
+    x_train = torch.linspace(1, 5, 101).to('cpu').unsqueeze(0).T
     y_train = process_tensor_with_function(x_train, function)
     y_train = y_train.squeeze()
 
@@ -26,17 +32,23 @@ if __name__ == '__main__':
     if 'parfam' in usage:
         from parfam.parfamwarpper import ParFamWrapper
         parfam = ParFamWrapper(iterate=True, functions=[lambda x: x], function_names=[sympy.Id])
+        start_time = time.time()
         parfam.fit(x_train, y_train, time_limit=100)
+        end_time = time.time()
         results["parfam"] = parfam.formula.simplify()
+        results["parfam_time"] = end_time - start_time
         print(parfam.formula.simplify())
 
     # ------------ MySR -------------- #
     if 'mysr' in usage:
         from mysr.RationalSR import RationalFunction
-        mySR = RationalFunction(2, 0)
+        mySR = RationalFunction(3)
+        start_time = time.time()
         mySR.fit(x_train.squeeze(), y_train)
+        end_time = time.time()
         mySr_function = mySR.get_function()
         results["mySR"] = mySr_function
+        results["mySR_time"] = end_time - start_time
         print(mySr_function)
 
     # ----------- GINN-LP ------------ #
@@ -44,8 +56,11 @@ if __name__ == '__main__':
         from ginnlp import GINNLP
         ginnLP = GINNLP(num_epochs=500, round_digits=3, start_ln_blocks=1, growth_steps=3,
                         l1_reg=1e-4, l2_reg=1e-4, init_lr=0.01, decay_steps=1000, reg_change=0.5)
-        ginnLP.fit(x_train.squeeze(), y_train)
-        results["ginnlp"] = ginnLP.recovered_eq
+        start_time = time.time()
+        ginnLP.fit(x_train, y_train)
+        end_time = time.time()
+        results["ginnLP"] = ginnLP.recovered_eq
+        results["ginnLP_time"] = end_time - start_time
         print(ginnLP.recovered_eq)
 
     # save results
--- tests/ginn_lp_test.py
+++ tests/ginn_lp_test.py
@@ -6,7 +6,7 @@ functions = [f'(2*x**2 + {torch.pi} * x + 3)/(x+7)',
              f'(x**2 + 2)/(x**2+1)']
 
 recovered_functions = []
-x_train_base = torch.linspace(-5, 5, 101).to('cpu').unsqueeze(0).T
+x_train_base = torch.linspace(1, 5, 101).to('cpu').unsqueeze(0).T
 for i, function in enumerate(functions):
     target_function = sp.lambdify('x', function)
     y_train = target_function(x_train_base)
@@ -16,7 +16,7 @@ for i, function in enumerate(functions):
     y_train = y_train[mask]
 
     ginnLP = GINNLP(num_epochs=500, round_digits=3, start_ln_blocks=1, growth_steps=3,
-                    l1_reg=1e-4, l2_reg=1e-4, init_lr=0.01, decay_steps=1000, reg_change=0.5)
+                    l1_reg=1e-4, l2_reg=1e-4, init_lr=0.01, decay_steps=1000, reg_change=0.5, train_iter=1)
     ginnLP.fit(x_train, y_train.squeeze())
     print("Functions learned: {0}".format(i+1))
     print(function)
