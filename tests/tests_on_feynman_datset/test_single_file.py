884512f5ddf661f1db0d08a792ebfbb13e187c58
mih <michael.hoelscher@qplix.com>
2025-02-03 16:11:54 +0100

    only run single file

---
+++ tests/tests_on_feynman_datset/test_single_file.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import pandas as pd
+
+from tests.tests_on_feynman_datset.parfam_on_feynmann import get_test_results_for_file as runParfam
+
+# Relative Paths
+dataset_dir = Path.cwd().parent.parent.joinpath('Datasets')
+data_dir = dataset_dir.joinpath('Feynman_with_units')
+name = 'I.12.4'
+
+
+if __name__ == '__main__':
+    function_infos = pd.read_csv(dataset_dir.joinpath('Feynmann_laurent_polynomials.csv'), sep=';')
+    data_dict = dict(zip(function_infos['Filename'], function_infos['Formula']))
+    target_formula = data_dict.get(name, "Formula not found")
+
+    columns = ['Filename', 'Target Formula', 'Recovered Formula', 'Last Error', 'Run Time']
+    result_table = pd.DataFrame(columns=columns)
+
+    test_result = [name, target_formula] + runParfam(name)
+    result_table.loc[len(result_table)] = test_result
+
+    result_table.to_csv('single_results.csv', index=False)
